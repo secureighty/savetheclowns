c691c1e07c9136643c4d35477441e4bd36124688
Alphactory <aedan.e.taylor@gmail.com>
2023-05-25 16:04:52 -0400

    date this man

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ RUN pip install -r requirements.txt
 
 COPY app.py .
 COPY embed .
+COPY burritoman.jpg .
 
 ENTRYPOINT ["flask", "run", "--host=0.0.0.0", "--port=8080"]
 
--- app.py
+++ app.py
@@ -3,8 +3,17 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
+    result = ""
     with open("embed") as f:
-        return f.read()
+        result += f.read()
+    result += "<br>"
+    result += "Please, save this poor clown. He's single. Contact Draco Blaze#0942 on Discord. <br>"
+    result += "<img src=/burritoman.jpg width=560>"
+    return result
+
+@app.route('/burritoman.jpg')
+def burritoman():
+    return flask.send_file("burritoman.jpg")
 
 app.run()
 
