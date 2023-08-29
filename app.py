c0b8742320193ce5b1325d5ef836953a5e0ce843
Alphactory <aedan.e.taylor@gmail.com>
2023-08-28 20:33:18 -0400

    added elle because she's also single

--- app.py
+++ app.py
@@ -9,11 +9,18 @@ def index():
     result += "<br>"
     result += "Please, save this poor clown. He's single. Contact dbcu on Discord. <br>"
     result += "<img src=/burritoman.jpg width=560>"
+    result += "<br>"
+    result += "And this one. She's also single. Contact soleilcrow on Discord <br>"
+    result += "<img src=/rockwoman.jpg width=560>"
     return result
 
 @app.route('/burritoman.jpg')
 def burritoman():
     return flask.send_file("burritoman.jpg")
+
+@app.route('/rockwoman.jpg')
+def rockwoman():
+    return flask.send_file("rockwoman.jpg")
 
 app.run()
 
